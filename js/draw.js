2b03d3948d7bfb7da35d1a5fa5ac522aab3e0f28
zhangss <zssqqzss@vip.qq.com>
2017-08-02 23:32:23 +0800

    add

---
+++ js/draw.js
@@ -0,0 +1,10 @@
+function draw(canvas, data) {
+    console.log(canvas);
+    console.log(data);
+
+    if (0) {
+        requestAnimationFrame(draw);
+    }
+}
+
+export default draw;
--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {data} from "./data.js"
+import draw from "./draw.js"
 
 var Conponent = React.createClass({
     render : function(){
         return (
             <div>
+                <p>rank:</p>
                 <canvas id="cv" width="800" height="800"></canvas>
             </div>
         )
@@ -17,7 +19,8 @@ ReactDOM.render(
     document.querySelector("#app")
 );
 
+console.log(cv);
 data().then(function(value){
     //这里处理数据
-    console.log(value);
+    draw(cv,value);
 })
